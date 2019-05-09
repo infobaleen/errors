cb435e191fe33de6238e6ba72df6da4cafe9b191
Florian Uekermann <florian@uekermann.me>
2019-05-09 18:07:33 +0200

    refactor finalizer and add scopes

--- cleanup.go
+++ cleanup.go
@@ -3,41 +3,54 @@ package errors
 import "log"
 
 type Finalizer struct {
-	error  []func() error
-	always []func() error
+	parent  *Finalizer
+	onScope callStack
+	onFinal callStack
 }
 
 func NewFinalizer() *Finalizer {
 	return new(Finalizer)
 }
 
-func (c *Finalizer) Finalize(err *error) {
-	var internalError = callStack(&c.always)
-	if internalError != nil {
-		internalError = WithAftermath(internalError, callStack(&c.error))
-	} else if err != nil && *err != nil {
-		internalError = WithAnother(internalError, callStack(&c.error))
+func (c *Finalizer) ChildScope() *Finalizer {
+	return &Finalizer{parent: c}
+}
+
+func (c *Finalizer) EndScope(err *error) {
+	var tmpErr error
+	for c.onScope != nil {
+		tmpErr = WithAnother(tmpErr, c.onScope.pop())
+	}
+	if tmpErr == nil && (err == nil || *err == nil) && c.parent != nil {
+		c.parent.onFinal = append(c.parent.onFinal, c.onFinal...)
+	} else {
+		for c.onFinal != nil {
+			tmpErr = WithAnother(tmpErr, c.onScope.pop())
+		}
 	}
 	if err != nil {
-		*err = WithAftermath(*err, internalError)
-	} else if internalError != nil {
-		log.Println("finalizer error:", internalError)
+		*err = WithAftermath(*err, tmpErr)
+	} else if tmpErr != nil {
+		log.Println("finalizer error:", tmpErr)
 	}
 }
 
-func callStack(stack *[]func() error) error {
-	var err error
-	for l := len(*stack); l > 0; l-- {
-		err = WithAnother(err, (*stack)[l-1]())
+type callStack []func() error
+
+func (c *callStack) pop() error {
+	var l = len(*c)
+	var f = (*c)[l-1]
+	*c = (*c)[:l-1]
+	if len(*c) == 0 {
+		*c = nil
 	}
-	*stack = nil
-	return err
+	return f()
 }
 
-func (c *Finalizer) ErrFn(f ...func() error) {
-	c.error = append(c.error, f...)
+func (c *Finalizer) OnScopeEnd(f ...func() error) {
+	c.onScope = append(c.onScope, f...)
 }
 
-func (c *Finalizer) FinalFn(f ...func() error) {
-	c.error = append(c.always, f...)
+func (c *Finalizer) OnFinalEnd(f ...func() error) {
+	c.onFinal = append(c.onFinal, f...)
 }
